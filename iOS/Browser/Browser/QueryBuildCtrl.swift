1610eae33c7b05db9f3e7adf571121bbda2057f6
Alex <alexander.poehlmann@hof-university.de>
2016-04-19 15:48:17 +0200

    QueryBuildCtrl aufgeräumt

--- iOS/Browser/Browser/QueryBuildCtrl.swift
+++ iOS/Browser/Browser/QueryBuildCtrl.swift
@@ -14,19 +14,10 @@ class QueryBuildCtrl {
         var searchQuery = [SearchQuery]()
         for searchModel in searchModels.getSearchModels() {
             var searchContexts = [SearchContext]()
-            
             for (key,value) in searchModel.tags {
                 var dic = value.getValues()
                 dic["tag"] = key
-
-                if (dic["type"]! as! String) == "" {
-                    dic["type"] = "Misc"
-                }else{
-//                    let str = dic["type"] as! String
-//                    dic["type"] = str.substringToIndex(str.startIndex.advancedBy(1)).uppercaseString + str.substringFromIndex(str.startIndex.advancedBy(1))
-                }
                 searchContexts.append(SearchContext(values: dic, filters: searchModel.filters.getValues()))
-            
             }
             searchQuery.append(SearchQuery(index: searchModel.index, searchContext: searchContexts,url: searchModel.url,title: searchModel.title))
         }
