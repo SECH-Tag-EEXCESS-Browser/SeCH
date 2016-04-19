c95e9943611011aa519765d977897525d545c794
burakerol <burak.erol@hof-university.de>
2016-04-19 17:06:48 +0200

    Added readHead.js

--- iOS/Browser/Browser/readHead.js
+++ iOS/Browser/Browser/readHead.js
@@ -1,22 +1,22 @@
 function returnHead(){
-var metas = document.getElementsByTagName("META");
-var i;
-for (i = 0; i < metas.length; i++) {
-    if (metas[i].getAttribute("name") == "search"){
-        var string = "";
-
-       
-        
-        string = string + "topic=" + "\"" + metas[i].getAttribute("topic") + "\" ";
-        string = string + "type=" + "\"" + metas[i].getAttribute("type") + "\" ";
-        string = string + "mediaType=" + "\"" + metas[i].getAttribute("mediaType") + "\" ";
-        string = string + "provider=" + "\"" + metas[i].getAttribute("provider") + "\" ";
-        string = string + "licence=" + "\"" + metas[i].getAttribute("licence") + "\" ";
-        
-        
-        return string;
+    var metas = document.getElementsByTagName("META");
+    var i;
+    for (i = 0; i < metas.length; i++) {
+        if (metas[i].getAttribute("name") == "search"){
+            var string = "";
+            
+            
+            
+            string = string + "topic=" + "\"" + metas[i].getAttribute("topic") + "\" ";
+            string = string + "type=" + "\"" + metas[i].getAttribute("type") + "\" ";
+            string = string + "mediaType=" + "\"" + metas[i].getAttribute("mediaType") + "\" ";
+            string = string + "provider=" + "\"" + metas[i].getAttribute("provider") + "\" ";
+            string = string + "licence=" + "\"" + metas[i].getAttribute("licence") + "\" ";
+            
+            
+            return string;
+        }
     }
-}
-return "Nothing found";
+    return "Nothing found";
 }
 returnHead();
