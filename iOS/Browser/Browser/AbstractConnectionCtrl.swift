4201c09ce89c30c651588fc3de7da7bed3a9cdb9
burakerol <burak.erol@hof-university.de>
2016-05-10 16:59:42 +0200

    Added Classes
    Added Classes

---
+++ iOS/Browser/Browser/AbstractConnectionCtrl.swift
@@ -0,0 +1,9 @@
+//
+//  AbstractConnectionCtrl.swift
+//  Browser
+//
+//  Created by Burak Erol on 10.05.16.
+//  Copyright © 2016 SECH-Tag-EEXCESS-Browser. All rights reserved.
+//
+
+import Foundation
---
+++ iOS/Browser/Browser/JsonConnectionCtrl.swift
@@ -0,0 +1,9 @@
+//
+//  JsonConnectionCtrl.swift
+//  Browser
+//
+//  Created by Burak Erol on 10.05.16.
+//  Copyright © 2016 SECH-Tag-EEXCESS-Browser. All rights reserved.
+//
+
+import Foundation
