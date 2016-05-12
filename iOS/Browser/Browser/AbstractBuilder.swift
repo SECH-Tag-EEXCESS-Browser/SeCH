bb5c60df486ce37dca66faaba69a3f7c5af0d88c
burakerol <burak.erol@hof-university.de>
2016-05-12 13:26:30 +0200

    Added New Classes

---
+++ iOS/Browser/Browser/AbstractBuilder.swift
@@ -0,0 +1,9 @@
+//
+//  AbstractBuilder.swift
+//  Browser
+//
+//  Created by Burak Erol on 12.05.16.
+//  Copyright © 2016 SECH-Tag-EEXCESS-Browser. All rights reserved.
+//
+
+import Foundation
---
+++ iOS/Browser/Browser/AbstractResponseParser.swift
@@ -0,0 +1,9 @@
+//
+//  AbstractResponseParser.swift
+//  Browser
+//
+//  Created by Burak Erol on 12.05.16.
+//  Copyright © 2016 SECH-Tag-EEXCESS-Browser. All rights reserved.
+//
+
+import Foundation
